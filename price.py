73fe5c7a616b31d62ad4d6b595a613adf3f04fd3
Akshay Kumar <akshay92k@gmail.com>
2026-01-09 23:23:12 -0800

    Restructuring Agent Layout

--- price.py
+++ price.py
@@ -1,17 +1,3 @@
-from dotenv import load_dotenv
-from langchain.tools import tool, ToolRuntime
-from langchain.agents import create_agent
-from langchain.messages import HumanMessage, SystemMessage
-from pprint import pprint
-
-load dotenv()
-
-@tool
-def get_available_gtins(gtin: [str]) -> [str]:
-    """Get the available GTINs for a category of products"""
-    
-
-
 #Code Structure : 
 #1. Identify various workflows.
 #2. Build corresponding agents against each workflow. Leverage MCP servers or build them wherever possible.
@@ -24,3 +10,54 @@ def get_available_gtins(gtin: [str]) -> [str]:
 #9. Build a central monitoring system that can be used to monitor the agents.
 #10. Build a central reporting system that can be used to report the activities.
 
+
+from dotenv import load_dotenv
+from langchain.tools import tool, ToolRuntime
+from langchain.agents import create_agent
+from langchain.messages import HumanMessage, SystemMessage
+from pprint import pprint
+from typing import Dict, Any
+from tavily import TavilyClient
+from langchain.tools import tool
+
+#Create Agent State
+from langchain.agents import AgentState
+
+class ProductState(AgentState):
+    product_id: str
+    product_description: str
+    product_price: float
+    product_availability: str
+    target_seller: str
+
+
+@tool
+def update_latest_gtin()->str:
+    """Update the latest GTIN for a category of products
+        #This function will identify a suitable source 
+        # of data and download all publicly available GTINs."""
+
+
+@tool
+def product_identifier_search(product_id:str)->str:
+    """Search for the product based on its identifier"""
+
+@tool
+def product_description_search(product_description:str)->str:
+    """Search for the product based on its description"""
+
+@tool
+def retailer_product_search(target_seller:str)->str:
+    """Search for the product in the target retailer"""
+
+@tool
+def identify_retailer(product_id:str)->str:
+    """Identify the retailer for the product"""
+
+@tool
+def marketplace_product_search(target_seller:str)->str:
+    """Search for the product in the target retailer"""
+
+@tool
+def identify_marketplace(product_id:str)->str:
+    """Identify the retailer for the product"""
